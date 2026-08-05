a01283827abbdb3c1d21aaeb9fcb11a6b4dd4077
Elahe <alale_66@yahoo.com>
2026-08-04 22:40:27 -0500

    - Correct the issue for loading mp4 file on illustration-collage.jsion

--- static/illustration-collage.js
+++ static/illustration-collage.js
@@ -1,10 +1,4 @@
 const illustrationImages = [
-    {
-        src: "/static/illustration/aram.mp4",
-        title: "Illustration Process",
-        slug: "digital-portrait",
-        isVideo: true
-    },
     {src: "/static/illustration/dyk-book-ils.jpg", title: "Did You Know?", slug: "did-you-know-book"},
     {
         src: "/static/illustration/father-card.mp4",
@@ -13,6 +7,12 @@ const illustrationImages = [
         isVideo: true
     },
     {src: "/static/illustration/nowruz2-book-ils.jpg", title: "Nowruz", slug: "nowruz-book"},
+    {
+        src: "/static/illustration/aram.mp4",
+        title: "Illustration Process",
+        slug: "digital-portrait",
+        isVideo: true
+    },
     {src: "/static/illustration/nowruz-card-ils.jpg", title: "Amoo Nowruz", slug: "amoo-nowruz-card"},
     {src: "/static/illustration/valentine-card-ils.jpg", title: "Happy Valentine's Day", slug: "happy-valentine's-day"},
     {src: "/static/illustration/nowruz-book-ils.jpg", title: "Nowruz", slug: "nowruz-book"},
